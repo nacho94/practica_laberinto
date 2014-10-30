765c9f4ab484f9d429186be10df1066c397325b4
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2014-10-30 18:52:50 +0100

    entrada

---
+++ laberinto.java
@@ -0,0 +1,22 @@
+
+
+public class laberinto {
+
+
+	public static void main (String [] args) {
+
+
+	}
+
+	public static int [][] pedirEntrada () {
+
+		int n = 5;
+		int [][] matriz = new int [n][];
+
+		for (int i = 0; i<n; i++) {
+			matriz [i] = new int [n];
+
+		}
+		return matriz;
+	}
+}
